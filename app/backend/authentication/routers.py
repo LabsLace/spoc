b2f72790f2803227e8db40d4628226acdd1f5bc3
Rodrigo Macedo <45414340+Macmaad@users.noreply.github.com>
2022-01-24 19:39:32 -0600

    Feature/auth users (#4)
    * Create user login and change user registration
    
    * remove logout

---
+++ app/backend/authentication/routers.py
@@ -0,0 +1,23 @@
+"""
+Router to manage the authentication of the users to the app.
+Some parts that we need to check are:
+1. how are we goingo to keep trak of the sessions?
+2. How is the frontend going to take care of the session?
+3. how will each request will be checking that the session is valid?
+4. How long will each session last?
+"""
+from fastapi import APIRouter, Depends
+from fastapi.security import OAuth2PasswordRequestForm
+
+from authentication.login import login
+from shared_functions.dependencies import manager
+
+router = APIRouter(tags=["authentication"])
+
+
+@router.post("/login/")
+def login_(data: OAuth2PasswordRequestForm = Depends()):
+    """
+    Handler to manage the login of the users.
+    """
+    return login(data)
--- app/backend/main.py
+++ app/backend/main.py
@@ -1,10 +1,13 @@
 """
 App initilizer.
 """
+
 from fastapi import FastAPI
 
 from users.routers import router as users_router
+from authentication.routers import router as auth_router
 
 app = FastAPI()
 
 app.include_router(users_router)
+app.include_router(auth_router)
